0b4d4974f8c2f96fc93bc3d2f8d47ec59c41bbd5
Subrata Sarker <subrata_sarker@yahoo.com>
2019-03-19 23:52:33 +0900

    withRouter can be  used on any component in next.js app

--- pages/post.js
+++ pages/post.js
@@ -1,11 +1,17 @@
 import { withRouter } from 'next/router';
 import Layout from '../components/MyLayout';
 
-const Page = withRouter(props => (
-    <Layout>
+const Content = withRouter(props => (
+    <div>
         <h1>{ props.router.query.title }</h1>
         <p>This is the blog post content for { props.router.query.title }</p>
-    </Layout>
+    </div>
 ));
+
+const Page = props => (
+    <Layout>
+        <Content />
+    </Layout>
+);
 
 export default Page;
