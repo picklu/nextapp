d40b87fbe36bef7f23bf7022515244e494ce5891
Subrata Sarker <subrata_sarker@yahoo.com>
2019-03-19 22:20:20 +0900

    Add link prop

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default () => (
     <div>
         <Link href="/">
-            <a>Home Page</a>
+            <a title="Home Page">Home Page</a>
         </Link>
         <p>This is the about page</p>
     </div>
