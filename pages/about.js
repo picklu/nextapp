80e5d12d7201f4cb814199f2a057ad9149c8b8db
Subrata Sarker <subrata_sarker@yahoo.com>
2019-03-19 21:32:53 +0900

    Navigate between pages

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default () => (
+    <div>
+        <Link href="/">
+            <a>Home Page</a>
+        </Link>
+        <p>This is the about page</p>
+    </div>
+)
